229bf8c6002cc2c5e768a3a5a4d2ef3df7be26db
danieltapasco <daniel.tapasco@ucp.edu.co>
2025-05-17 16:27:43 -0500

    Initial commit UCP

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Estudiante: Daniel Tapasco Trejos</p>
+        <p>Estudiante: Daniel Tapasco</p>
         <a
           className="App-link"
           href="https://reactjs.org"
